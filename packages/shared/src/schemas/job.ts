a9bd1e9011118590beceb42915a7d7a96b55b920
Kyrylo Savieliev <kirillofficial0507@gmail.com>
2024-10-26 12:52:52 +0300

    feat(shared): add schemas and types for jobs module

--- packages/shared/src/schemas/job.ts
+++ packages/shared/src/schemas/job.ts
@@ -19,15 +19,27 @@ const JOB_SCHEMA = BASE_MODEL.extend({
   areStudentsAllowed: z.boolean(),
   isActive: z.boolean(),
   isHidden: z.boolean(),
-  categoryId: z.number(),
-  positionId: z.number(),
-  employerId: z.number(),
-  cityId: z.number(),
+  categoryId: z.coerce.number(),
+  positionId: z.coerce.number(),
+  companyId: z.coerce.number(),
+  cityId: z.coerce.number(),
   salary: JOB_SALARY_SCHEMA,
   skills: JOB_SKILL_SCHEMA.array(),
 })
 
 type Job = z.infer<typeof JOB_SCHEMA>
 
-export type { Job }
-export { JOB_SCHEMA }
+const CREATE_JOB_SCHEMA = JOB_SCHEMA.omit({
+  companyId: true,
+  id: true,
+  updatedAt: true,
+  createdAt: true,
+  isActive: true,
+  isHidden: true,
+  skills: true,
+})
+
+type CREATE_JOB_SCHEMA_TYPE = z.infer<typeof CREATE_JOB_SCHEMA>
+
+export type { Job, CREATE_JOB_SCHEMA_TYPE }
+export { JOB_SCHEMA, CREATE_JOB_SCHEMA }
