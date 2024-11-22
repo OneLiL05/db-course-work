df3d1c6dc6631af3904ff460f8554323b5306d9e
Kyrylo Savieliev <kirillofficial0507@gmail.com>
2024-11-22 13:43:53 +0200

    refactor(server): refactor some queries using jobs view

--- apps/server/src/modules/jobs/repositories/JobRepository.ts
+++ apps/server/src/modules/jobs/repositories/JobRepository.ts
@@ -1,117 +1,31 @@
 import {
   DatabaseClient,
-  categories,
-  cities,
-  companies,
   jobSalaries,
   jobSkills,
   jobs,
-  positions,
-  skillLevels,
-  skills,
+  jobsView,
 } from '@skill-swap/db'
 import { CREATE_JOB_SCHEMA_TYPE, Job } from '@skill-swap/shared'
+import { and, between, count, eq } from 'drizzle-orm'
 import { IJobRepository } from '../interfaces/index.js'
 import { JobsInjectableDependencies } from '../types/index.js'
-import { eq, getTableColumns, sql, count, and, between } from 'drizzle-orm'
-import { SqlClient } from '@/types/index.js'
 
 export class JobRepository implements IJobRepository {
   private readonly db: DatabaseClient
-  private readonly sql: SqlClient
 
-  constructor({ db, sql }: JobsInjectableDependencies) {
+  constructor({ db }: JobsInjectableDependencies) {
     this.db = db.client
-    this.sql = sql
   }
 
   async findMany(): Promise<Job[]> {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { categoryId, cityId, companyId, positionId, ...rest } =
-      getTableColumns(jobs)
-
-    return this.db
-      .select({
-        ...rest,
-        salary:
-          sql`json_build_object('amount', ${jobSalaries.amount}, 'currency', ${jobSalaries.currency})`.as(
-            'salary',
-          ),
-        skills:
-          sql`json_agg(json_build_object('name', ${skills.name}, 'level', ${skillLevels.name}))`.as(
-            'skills',
-          ),
-      })
-      .from(jobs)
-      .leftJoin(jobSalaries, eq(jobs.id, jobSalaries.jobId))
-      .leftJoin(jobSkills, eq(jobs.id, jobSkills.id))
-      .leftJoin(skills, eq(jobSkills.skillId, skills.id))
-      .leftJoin(skillLevels, eq(jobSkills.skillLevelId, skillLevels.id))
-      .groupBy(
-        jobs.id,
-        jobSalaries.amount,
-        jobSalaries.currency,
-      ) as unknown as Job[]
+    return this.db.select().from(jobsView) as unknown as Job[]
   }
 
   async findOne(id: number): Promise<Job | null> {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { categoryId, cityId, companyId, positionId, ...rest } =
-      getTableColumns(jobs)
-
-    // TODO: Move this query to the separate view
     const result = this.db
-      .select({
-        ...rest,
-        company:
-          sql`json_build_object('id', ${companies.id}, 'name', ${companies.name}, 'description', ${companies.description}, 'img', ${companies.img}, 'is_verified', ${companies.isVerified})`.as(
-            'company',
-          ),
-        city: sql`json_build_object('id', ${cities.id}, 'name', ${cities.name})`.as(
-          'category',
-        ),
-        category:
-          sql`json_build_object('id', ${categories.id}, 'name', ${categories.name})`.as(
-            'category',
-          ),
-        position:
-          sql`json_build_object('id', ${positions.id}, 'name', ${positions.name})`.as(
-            'position',
-          ),
-        salary:
-          sql`json_build_object('amount', ${jobSalaries.amount}, 'currency', ${jobSalaries.currency})`.as(
-            'salary',
-          ),
-        skills:
-          sql`json_agg(json_build_object('name', ${skills.name}, 'level', ${skillLevels.name}))`.as(
-            'skills',
-          ),
-      })
-      .from(jobs)
-      .leftJoin(companies, eq(jobs.companyId, companies.id))
-      .leftJoin(positions, eq(jobs.positionId, positions.id))
-      .leftJoin(cities, eq(jobs.cityId, cities.id))
-      .leftJoin(categories, eq(jobs.categoryId, categories.id))
-      .leftJoin(jobSalaries, eq(jobs.id, jobSalaries.jobId))
-      .leftJoin(jobSkills, eq(jobs.id, jobSkills.id))
-      .leftJoin(skills, eq(jobSkills.skillId, skills.id))
-      .leftJoin(skillLevels, eq(jobSkills.skillLevelId, skillLevels.id))
-      .where(eq(jobs.id, id))
-      .groupBy(
-        jobs.id,
-        jobSalaries.amount,
-        jobSalaries.currency,
-        companies.id,
-        companies.name,
-        companies.img,
-        companies.description,
-        positions.id,
-        positions.name,
-        cities.id,
-        cities.name,
-        categories.id,
-        categories.name,
-      ) as unknown as Job[]
+      .select()
+      .from(jobsView)
+      .where(eq(jobsView.id, id)) as unknown as Job[]
 
     const job = result.at(0)
 
@@ -121,70 +35,10 @@ export class JobRepository implements IJobRepository {
   }
 
   async findCompanyJobs(companyId: number): Promise<Job[]> {
-    const {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      categoryId,
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      cityId,
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      companyId: cId,
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      positionId,
-      ...rest
-    } = getTableColumns(jobs)
-
     return this.db
-      .select({
-        ...rest,
-        company:
-          sql`json_build_object('id', ${companies.id}, 'name', ${companies.name}, 'description', ${companies.description}, 'img', ${companies.img}, 'is_verified', ${companies.isVerified})`.as(
-            'company',
-          ),
-        city: sql`json_build_object('id', ${cities.id}, 'name', ${cities.name})`.as(
-          'category',
-        ),
-        category:
-          sql`json_build_object('id', ${categories.id}, 'name', ${categories.name})`.as(
-            'category',
-          ),
-        position:
-          sql`json_build_object('id', ${positions.id}, 'name', ${positions.name})`.as(
-            'position',
-          ),
-        salary:
-          sql`json_build_object('amount', ${jobSalaries.amount}, 'currency', ${jobSalaries.currency})`.as(
-            'salary',
-          ),
-        skills:
-          sql`json_agg(json_build_object('name', ${skills.name}, 'level', ${skillLevels.name}))`.as(
-            'skills',
-          ),
-      })
-      .from(jobs)
-      .leftJoin(companies, eq(jobs.companyId, companies.id))
-      .leftJoin(positions, eq(jobs.positionId, positions.id))
-      .leftJoin(cities, eq(jobs.cityId, cities.id))
-      .leftJoin(categories, eq(jobs.categoryId, categories.id))
-      .leftJoin(jobSalaries, eq(jobs.id, jobSalaries.jobId))
-      .leftJoin(jobSkills, eq(jobs.id, jobSkills.id))
-      .leftJoin(skills, eq(jobSkills.skillId, skills.id))
-      .leftJoin(skillLevels, eq(jobSkills.skillLevelId, skillLevels.id))
-      .where(eq(jobs.companyId, companyId))
-      .groupBy(
-        jobs.id,
-        jobSalaries.amount,
-        jobSalaries.currency,
-        companies.id,
-        companies.name,
-        companies.img,
-        companies.description,
-        positions.id,
-        positions.name,
-        cities.id,
-        cities.name,
-        categories.id,
-        categories.name,
-      ) as unknown as Job[]
+      .select()
+      .from(jobsView)
+      .where(eq(jobsView.companyId, companyId)) as unknown as Job[]
   }
 
   async findLatestCount(companyId: number): Promise<{ count: number } | null> {
