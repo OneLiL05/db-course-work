e25c7335c5ebe7f5eae5e33e6ce4130351b5ce60
Kyrylo Savieliev <kirillofficial0507@gmail.com>
2024-11-27 18:50:43 +0200

    feat(shared): add schema and type for company admin model

--- packages/shared/src/schemas/company.ts
+++ packages/shared/src/schemas/company.ts
@@ -2,6 +2,8 @@ import { z } from 'zod'
 import { BASE_MODEL } from './common.js'
 import { ID_SCHEMA } from './index.js'
 
+const COMPANY_ROLES = z.enum(['owner', 'admin'])
+
 const COMPANY_MODEL = BASE_MODEL.extend({
   description: z.string(),
   img: z.string(),
@@ -10,6 +12,16 @@ const COMPANY_MODEL = BASE_MODEL.extend({
 })
 
 type Company = z.infer<typeof COMPANY_MODEL>
+
+const COMPANY_ADMIN_MODEL = z.object({
+  id: z.number(),
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z.string().email(),
+  role: COMPANY_ROLES,
+})
+
+type CompanyAdmin = z.infer<typeof COMPANY_ADMIN_MODEL>
 
 const CREATE_COMPANY_SCHEMA = z.object({
   name: z.string().min(4).max(128),
@@ -27,9 +39,16 @@ type CREATE_COMPANY_ADMIN_SCHEMA_TYPE = z.infer<
   typeof CREATE_COMPANY_ADMIN_SCHEMA
 >
 
-export { COMPANY_MODEL, CREATE_COMPANY_SCHEMA, CREATE_COMPANY_ADMIN_SCHEMA }
+export {
+  COMPANY_MODEL,
+  CREATE_COMPANY_SCHEMA,
+  CREATE_COMPANY_ADMIN_SCHEMA,
+  COMPANY_ROLES,
+  COMPANY_ADMIN_MODEL,
+}
 export type {
   Company,
+  CompanyAdmin,
   CREATE_COMPANY_SCHEMA_TYPE,
   CREATE_COMPANY_ADMIN_SCHEMA_TYPE,
 }
